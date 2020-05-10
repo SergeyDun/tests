838546fdb51596023086f302ed713183068b7a9e
Sergey <56402515+SergeyDun@users.noreply.github.com>
2020-05-10 22:22:47 +0300

    Add files via upload

---
+++ JustForTest/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustForTest
+{
+    public class Program
+    {
+        public static int Min(int a, int b, int c)
+        {
+            int min = a;
+            if (min > b) min = b;
+            if (min > c) min = c;
+            return min;
+        }
+
+        public static bool IsTrue(int a, int b)
+        {
+            if(a + b < 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static object IsNull(int a, int b)
+        {
+            if (a + b < 0)
+            {
+                object h = null;
+                return h;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public static void Main(string[] args)
+        {
+            
+        }
+        
+    }
+}
